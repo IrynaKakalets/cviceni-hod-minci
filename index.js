9e99b2baca47c080c1d78a33cfaf41c32d287c4e
Iryna Kakalets <irinakakalec@gmail.com>
2024-03-30 21:57:05 +0100

    hotove

---
+++ index.js
@@ -0,0 +1,9 @@
+const padlOrel = Math.random() < 0.5;
+
+if (padlOrel) {
+  document.querySelector(".vysledek").textContent ="WINNER"
+  document.querySelector(".mince").classList.add("mince--orel")
+} else {
+  document.querySelector(".vysledek").textContent ="Try again"
+  document.querySelector(".mince").classList.add("mince--panna")
+}
